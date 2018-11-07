77353f3b8905d3dac006a4da4398b3802ea319f5
garyCoffey <garycoffey12@gmail.com>
2018-11-07 11:13:44 -0600

    WIP

--- src/services/PostData.js
+++ src/services/PostData.js
@@ -1,6 +1,6 @@
 export function PostData(type, userData) {
-  let BaseURL = 'https://apipaypal.9lessons.info/apipaypal/';
-  //let BaseURL = 'http://localhost/socialapi/';
+  let BaseURL = 'https://enigmatic-river-78551.herokuapp.com/';
+
 
   return new Promise((resolve, reject) => {
     fetch(BaseURL + type, {
